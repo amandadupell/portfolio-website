88cee79688fd4e6189e2ce8de4d1bd4cea15c921
Amanda Dupell <anddupell@gmail.com>
2023-04-05 14:04:36 -0400

    added some components and assets foundation

---
+++ src/assets/colors.ts
@@ -0,0 +1,3 @@
+export const $black = '#000000';
+export const $orange = '#EE9553';
+export const $white = '#FFFFFF';
---
+++ src/components/textComponent/textComponent.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const TextComponent = () => {
+    return ( <></>
+    )
+}
+
+export default TextComponent
