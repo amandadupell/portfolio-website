bf6c7f2bf6d3d19e80bd3b0f5349235093c45cd8
Amanda Dupell <anddupell@gmail.com>
2023-04-23 18:45:54 -0400

    added letterboxed image desc

--- src/images/letterboxd/imagedescriptions.ts
+++ src/images/letterboxd/imagedescriptions.ts
@@ -1,7 +1,14 @@
-export const letterboxdCoverImageDesc = "";
-export const exisitingBrandImage = "";
-export const userFeedbackImageDesc = "";
-export const signUpFlowImageDesc = "";
-export const homepageFlowImageDesc = "";
-export const signUpFinalImageDesc = "";
-export const homeFinalImageDesc = "";
+export const letterboxdCoverImageDesc =
+  "An image diplaying several screens and information regarding the current brand of the letterboxd application.";
+export const exisitingBrandImage =
+  "The image shows four circles with the brand colors for letterboxd: a vibrant orange, bright lime-green, deep sky-blue, and charcoal black.";
+export const userFeedbackImageDesc =
+  "A pie chart graphic that shows 50% of users noted a lack of movie recommendations, 30% of users said certain features where hidden, and 20% of users stated it was hard to find other users and new movies. ";
+export const signUpFlowImageDesc =
+  "A flow chart showing the progression of steps it takes for a user to sign up for the application. In this new flow, users will select genres they are interested in and then will be able to follow users based on those preferences.";
+export const homepageFlowImageDesc =
+  "A flow chart showing the progression of scrolling on the hompage. In this new flow, users are given more movie recommendations, lists to follows, reviews to engage with, and users to follow.";
+export const signUpFinalImageDesc =
+  "The final designs for the sign up flow, composed of 3 new screens. The designs included new components such as genre pill selectors and user cards.";
+export const homeFinalImageDesc =
+  "The final design for the home page flow. The new design includes several new components and sections such as a featured movies carousel, genre pills, popular reviews, and suggested users.";
